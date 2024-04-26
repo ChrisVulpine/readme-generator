252c8b802d1d509ad7ac0895b8c9924b7ed4aed5
Chris <157438747+ChrisVulpine@users.noreply.github.com>
2024-04-26 00:10:10 -0400

    added functions to write the readme file and also moved that function from index.js to generateMarkdown.js also added functions for license badges and information links.

--- Develop/index.js
+++ Develop/index.js
@@ -1,93 +1,78 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const chalk = require('chalk');
 const readmeGen = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
     const questions = [
     {
+        type: 'confirm',
+        name: 'intro',
+        message: chalk.green(`Hello! I'm your personal README generator helper. ^-^ Are you ready to create your awesome README file?`)
+    },
+    {
         type: 'input',
         name: 'title',
-        message: 'Enter your project title:'
+        message: `I am going to assume you said yes! Who would say no to the first prompt anyways? -_-; Please Enter your project title:`
     },
     {   type: 'input',
         name: 'Description',
-        message: 'Describe your project:'
+        message: chalk.green(`That's an amazing title! Now, describe your project:`)
     },
     {   type: 'input',
         name: 'Installation_Instructions',
-        message: 'Provide installation instructions:'
+        message: `Wow! Your project sounds incredible! Now provide installation instructions:`
     },
     {   type: 'input',
         name: 'Usage_Information',
-        message: 'Provide Usage Information:'
+        message: chalk.green(`You are doing great! Please provide the Usage Information:`)
     },
     {   type: 'input',
         name: 'Contribution_Guidelines',
-        message: 'Provide Contribution Guidelines:'
+        message: `Nice! How can people contribute to your project? Provide Contribution Guidelines:`
     },
     {   type: 'input',
         name: 'Test_Instructions',
-        message: 'Provide Test Instructions:'
+        message: chalk.green(`I'm sure your code has no bugs...but if it did, please provide Test Instructions:`)
     },
-    {   type: 'checkbox',
-        name: 'License',
-        message: 'Choose your license from the options below:',
-        choices: ['MIT', 'Potato', 'Other'],
-    },
-
-    {   type: 'input',
-        name: 'GitHub User Name',
-        message: 'What is your GitHub User Name?'
+    {
+        type: 'list',
+        name: 'license',
+        message: `Legal time! Please select a license to use for your project (I've provided the most common ones.):`,
+        choices: ['Apache 2.0', 'BSD 2-Clause', 'BSD 3-Clause', 'GNU AGPLv3.0', 'GNU GPLv2.0', 'GNU GPLv3.0', 'MIT', 'Mozilla Public 2.0'],
     },
 
     {   type: 'input',
-        name: 'Email Address',
-        message: 'What is your email address?'
+        name: 'githubUser',
+        message: chalk.green('What is your GitHub User Name?')
+    },
+
+    {   type: 'input',
+        name: 'email',
+        message: `I'll be sure to follow you! What is your email address?`
     },
 ]
 
 
 // TODO: Create a function to write README file
-function writeToFile() {
-    inquirer.prompt(questions)
-    .then((data)=> {
-    
-    const filename = `README.md`;
-    const content = `# ${data.title}
-
-## Description
-${data.Description}
-
-## Installation Instructions
-${data.Installation_Instructions}
-
-## Usage Information
-${data.Usage_Information}
-
-## Contribution Guidelines
-${data.Contribution_Guidelines}
-
-## Test Instructions
-${data.Test_Instructions}
-
-## License
-${data.License}
-
-## Questions
-${data.Questions}
-
-        `;
+function writeToFile(filename, content) {
+    // const markdownContent = readmeGen.generateMarkdown(data);
+//     const content = `# ${data.title}
 
     fs.writeFile(filename, content, (err) =>
-    err ? console.log(err) : console.log('Your Readme has been generated ^-^ !')
+    err ? console.log(err) : console.log('Nice work! Your Readme has been generated ^-^ Enjoy!')
     );
-})
 };
 
 // TODO: Create a function to initialize app
 function init() {
-    writeToFile();
+    inquirer.prompt(questions)
+    .then((answers) => {
+        const createReadme = readmeGen(answers);
+        writeToFile('README.md', createReadme);
+    })
+    
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,103 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // console.log("working!");
+  switch (license) {
+    case 'Apache 2.0':
+      return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    case 'BSD 2-Clause':
+      licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+      break;
+    case 'BSD 3-Clause':
+      licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+      break;
+    case 'GNU AGPLv3.0':
+      licenseBadge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+      break;
+    case 'GNU GPLv2.0':
+      licenseBadge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+      break;
+    case 'GNU GPLv3.0':
+      licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+      break;
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case 'Mozilla Public 2.0':
+      licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+      break;
+    default:
+      return '';
+      break;
+}
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return '[MIT](https://opensource.org/licenses/MIT)';
+      break;
+    case 'Apache':
+      return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
+    case 'GPL v3':
+      return '[GPL v3](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'MPL 2.0':
+      return '[MPL 2.0](https://opensource.org/licenses/MPL-2.0)';
+    case 'BSD 3':
+      return '[BSD 3-Clause](https://opensource.org/licenses/BSD-3-Clause)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `This project is covered under the ${renderLicenseLink(license)} license.
+  
+  ${renderLicenseBadge(license)}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-`;
+  return `# ${data.title} 
+
+  ## Description
+  ${data.Description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Credits](#credits)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.Installation_Instructions}
+  
+  ## Usage
+  ${data.Usage_Information}
+  
+  ## Contribution
+  ${data.Contribution_Guidelines}
+  
+  ## Tests
+  ${data.Test_Instructions}
+  
+  ## License
+  ${renderLicenseSection(data.license)}
+  
+  ## Questions
+  Got questions? Use the info below to contact me!<br>
+  GitHub: [@${data.githubUser}](https://github.com/${data.githubUser}/)\n\nEmail: [${data.email}](mailto:${data.email})\n
+  
+          `;
 }
 
 module.exports = generateMarkdown;
