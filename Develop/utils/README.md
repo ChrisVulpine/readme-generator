f92b5541c8eb67082b0f665458a8cd5fcda6ee12
Chris <157438747+ChrisVulpine@users.noreply.github.com>
2024-04-28 15:51:19 -0400

    Added readme to project and organized license links and info.

---
+++ Develop/utils/README.md
@@ -0,0 +1,58 @@
+# README Generator 
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+  [![Static Badge](https://img.shields.io/badge/Github-ChrisVulpine-darkgreen?style=flat&logo=github)](https://github.com/ChrisVulpine)
+
+  ## Description
+  This project will be used to generate professional quality README files using Microsoft's VS Code's README as a guideline. All necessary information including badges will be able to be generated through this command-line application.
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Credits](#credits)
+  - [Questions](#questions)
+  
+  ## Installation
+  Clone this repository to your device and install the dependencies using an npm install.
+  
+  ## Usage
+  After navigating to the project, run the following command: 
+  ``` 
+  node index.js.
+  ```
+   Prompts will help you gather all necessary information for the different sections of your README. Once the questions are answered, the README will be generated in the same directory as the index.js file. 
+  
+  ## Contribution
+  This application is not open for contribution at this time.
+  
+  ## Tests
+  This project has no tests to run at this time.
+
+  ## Credits
+* Top 5 software licenses: https://muratkaragozgil.medium.com/software-licenses-on-github-which-one-should-you-choose-3d4cfbb6c2f9
+
+* Inquirer Chalk: https://www.npmjs.com/package/inquirer-chalk-pipe
+
+* Inquirer NPM: https://www.npmjs.com/package/inquirer
+
+* General Reference: https://www.w3schools.com/
+
+* Readme Badge Generation: https://shields.io/
+
+* License Details: https://opensource.org/ & https://www.gnu.org/ 
+
+* Professional README guide: https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+
+
+## License
+This project is covered under the [MIT](https://opensource.org/licenses/MIT) license.
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Questions
+Got questions? Use the info below to contact me!<br>
+GitHub: [@ChrisVulpine](https://github.com/ChrisVulpine/)<br>
+Email: [Chrisleach1191@gmail.com](mailto:Chris1191@gmail.com)
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,32 +3,24 @@ function renderLicenseBadge(license) {
 
   switch (license) {
     case 'Apache 2.0':
-      return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-      break;
-    case 'BSD 2-Clause':
-      licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
-      break;
+      return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
     case 'BSD 3-Clause':
-      licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+      licenseBadge = `![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]`;
       break;
     case 'GNU AGPLv3.0':
-      licenseBadge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
-      break;
-    case 'GNU GPLv2.0':
-      licenseBadge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+      licenseBadge = `![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)]`;
       break;
     case 'GNU GPLv3.0':
-      licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+      licenseBadge = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`;
       break;
     case 'MIT':
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
       break;
     case 'Mozilla Public 2.0':
-      licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+      licenseBadge = `![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]`;
       break;
     default:
       return '';
-      break;
 }
 };
 
@@ -36,20 +28,26 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
 
   switch (license) {
-    case 'MIT':
-      return '[MIT](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return `[Apache 2.0](https://opensource.org/licenses/Apache-2.0)`;
+    case 'BSD 3-Clause':
+      licenseBadge = `[BSD 3-Clause](https://opensource.org/licenses/BSD-3-Clause)`;
       break;
-    case 'Apache':
-      return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
-    case 'GPL v3':
-      return '[GPL v3](https://www.gnu.org/licenses/gpl-3.0)';
-    case 'MPL 2.0':
-      return '[MPL 2.0](https://opensource.org/licenses/MPL-2.0)';
-    case 'BSD 3':
-      return '[BSD 3-Clause](https://opensource.org/licenses/BSD-3-Clause)';
+    case 'GNU AGPLv3.0':
+      licenseBadge = `[GNU AGPLv3.0](https://www.gnu.org/licenses/agpl-3.0)`;
+      break;
+    case 'GNU GPLv3.0':
+      licenseBadge = `[GNU GPLv3.0](https://www.gnu.org/licenses/gpl-3.0)`;
+      break;
+    case 'MIT':
+      return `[MIT](https://opensource.org/licenses/MIT)`;
+      break;
+    case 'Mozilla Public 2.0':
+      licenseBadge = `[Mozilla Public 2.0](https://opensource.org/licenses/MPL-2.0)`;
+      break;
     default:
       return '';
-  }
+}
 }
 
 // Function that returns the license section of README
@@ -61,41 +59,40 @@ function renderLicenseSection(license) {
 
 // Function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} 
-  ${renderLicenseSection(data.license)}
-  ## Description
-  ${data.Description}
-  
-  ## Table of Contents
-  
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Contribution](#contribution)
-  - [Tests](#tests)
-  - [License](#license)
-  - [Credits](#credits)
-  - [Questions](#questions)
-  
-  ## Installation
-  ${data.Installation_Instructions}
-  
-  ## Usage
-  ${data.Usage_Information}
-  
-  ## Contribution
-  ${data.Contribution_Guidelines}
-  
-  ## Tests
-  ${data.Test_Instructions}
-  
-  ## License
-  ${renderLicenseSection(data.license)}
-  
-  ## Questions
-  Got questions? Use the info below to contact me!<br>
-  GitHub: [@${data.githubUser}](https://github.com/${data.githubUser}/)\n\nEmail: [${data.email}](mailto:${data.email})\n
-  
-          `;
+return `# ${data.title} 
+${renderLicenseBadge(data.license)}
+## Description
+${data.Description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Credits](#credits)
+- [Questions](#questions)
+
+## Installation
+${data.Installation_Instructions}
+
+## Usage
+${data.Usage_Information}
+
+## Contribution
+${data.Contribution_Guidelines}
+
+## Tests
+${data.Test_Instructions}
+
+## License
+${renderLicenseSection(data.license)}
+
+## Questions
+Got questions? Use the info below to contact me!<br>
+GitHub: [@${data.githubUser}](https://github.com/${data.githubUser}/)\n\n <br>
+Email: [${data.email}](mailto:${data.email})\n`;
 }
 
 module.exports = generateMarkdown;
